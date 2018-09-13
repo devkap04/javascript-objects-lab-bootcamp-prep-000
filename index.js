2fc5a008d00f81af0db06a10ed82ea216ab3b5e1
devinkapelanski@yahoo.com <devinkapelanski@yahoo.com>
2018-09-13 06:44:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign(object, {[key] : value})
+  return Object.assign(object, {[key] : value});
 }
 
-function destructivelyDeleteFromObjectByKey(object, key) {
+function deleteFromObjectByKey(object, key) {
   
 }
